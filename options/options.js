e769d358415df8158dc536b98f7313b5c6fb8529
Crowdbotics <team@crowdbotics.com>
2021-08-29 01:54:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "rushee_30170Identifier",
+  url: "https://rushee_30170Identifier.botics.co",
+  api: "https://rushee_30170Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
